7dc27a3867b2663c8e82be11ea947e2c4817fcfa
Fiona Lapham <fionadlapham@gmail.com>
2018-02-21 14:53:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,15 @@
 require 'pry'
 
 class CashRegister
+  #each .new is a transaction
+  #each transaction begins with a total of 0, a possible discount, an ability to call the last transaction, and an empty array of items
 
-  attr_accessor :total, :discount, :last_transaction
-  @@totals = 0
+  attr_accessor :total, :discount, :last_transaction, :items
 
   def initialize (discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item (title, price, quantity=1)
