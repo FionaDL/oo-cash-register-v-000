0b7a7cef269f0e0cfbbeaee4728e91fc428a81eb
Fiona Lapham <fionadlapham@gmail.com>
2018-02-21 15:05:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,16 +9,17 @@ class CashRegister
   def initialize (discount = 0)
     @total = 0
     @discount = discount
-    @items = []
+    
   end
 
   def add_item (title, price, quantity=1)
     self.total =+ price * quantity
+    
   end
 
   def apply_discount
     if @discount != 0
-      discount_total = discount/100 * @total
+      discount_total = discount/100.0 * @total
       discount_total1 = @total - discount_total
       "After the discount, the total comes to $#{discount_total1}."
       binding.pry
